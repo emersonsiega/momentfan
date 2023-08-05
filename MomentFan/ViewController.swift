29ce73fc377496c8968f0f674d3ade4a2cff5210
Emerson Siega <emerson.siega@genial.com.vc>
2023-08-05 11:25:09 -0300

    Initial Commit

---
+++ MomentFan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MomentFan
+//
+//  Created by emersonsiega on 05/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
